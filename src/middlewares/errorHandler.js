ffa7008281a08ff7acb4c2f96d15f79358b905c5
Nhudeptrai <thehy109@gmail.com>
2026-08-23 12:49:09 +0700

    feature: ErrorHandler & RequestLogger

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+
+    const statusCode = err.statusCode || 500;
+
+    console.error("ERROR:", err);
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+    });
+};
+
+module.exports = errorHandler;
---
+++ src/middlewares/requestLogger.js
@@ -0,0 +1,8 @@
+const requestLogger = (req, res, next) => {
+    const time = new Date().toISOString();
+
+    console.log(`[${time}] ${req.method} ${req.originalUrl}`);
+    next();
+};
+
+module.exports = requestLogger;
